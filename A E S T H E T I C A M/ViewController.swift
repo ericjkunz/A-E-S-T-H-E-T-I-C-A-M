d1d43dce830e4901ac8953a9e4799dc015cb3a69
Nick Lee <npl148@gmail.com>
2016-05-21 10:47:17 -0400

    Initial Commit

---
+++ A E S T H E T I C A M/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A E S T H E T I C A M
+//
+//  Created by Nick Lee on 5/21/16.
+//  Copyright © 2016 Nick Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
